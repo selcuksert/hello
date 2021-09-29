524092a169d3b3238b18eb4e0f3bae43434f2a11
Selcuk SERT <selcuk.sert@gmail.com>
2021-09-29 21:18:33 +0300

    Update hello-openshift-for-developers.go

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -10,8 +10,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		currentTime := time.Now().Format("01-02-2006 15:04:05")
-		
+		var currentTime = time.Now().Format("01-02-2006 15:04:05")
 		response = "Hello OpenShift Developers! ["+currentTime+"]"
 	}
 
